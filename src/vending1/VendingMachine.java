151ed2b969d8759bc01aee95ec9bec49b9eff7a0
John Russo <jrusso440@gmail.com>
2023-11-10 10:00:59 -0500

    11-10

--- src/vending1/VendingMachine.java
+++ src/vending1/VendingMachine.java
@@ -45,14 +45,58 @@ public class VendingMachine {
          return tcbox.getBalance();
      }
      
+    public double findProduct(int pid)
+    {
+        double pr = -1;
+        for (int i = 0;i<lpr.length;i++)
+        {
+            if (lpr[i].getProductID() == pid)
+            {
+                pr=lpr[i].getPrice();
+                break;
+            }
+        }
+        return pr;
+    }
+    
+     
+     public boolean dispenseProduct(int pid)
+     {
+         
      /*to do method to select product 
      should include check to make sure we can pay for this and
      make change
-     1. pass in product id
+     1. pass in product id done
      2. get price of product
+           do we have the product in our local inventory?
+            if not return false if so get price
      3. assuming user insert money check against temp cb balance
         validateChange method
      4. makeChange method
      5. update inventory (to be written inside of product and vendingmachine)
      */
+     double prodPrice = findProduct(pid);
+     if (prodPrice > 0)
+     {
+         //check price against what user put in for money
+         if (prodPrice >= tcbox.getBalance())
+                 {
+                     /*can I make change?*/
+                     if (cbox.validateChange(tc.balance(), prodPrice))
+                     {
+                         //make change
+                         //call cashbox method to make change - update cashbox and also return a cashbox
+                         //update quantity
+                         //call update quantity method
+                     }
+                     else
+                         return false;
+                 }
+         else
+             return false;
+     }
+     else
+         return false;
+     return true;
+    }
 }
