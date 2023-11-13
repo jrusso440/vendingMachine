770dc1f7eee4eadf98f854b623b5ac0b31d7409f
JohnRusso <JohnRusso@host.docker.internal>
2023-11-13 14:54:17 -0500

    changes 11-13

--- src/vending1/VendingMachine.java
+++ src/vending1/VendingMachine.java
@@ -53,6 +53,49 @@ public class VendingMachine {
      3. assuming user insert money check against temp cb balance
         validateChange method
      4. makeChange method
+     5. update inventory (to be wcmritten inside of product and vendingmachine)
+     */
+     
+      public boolean dispenseProduct(int pid)
+     {
+         
+     /*to do method to select product 
+     should include check to make sure we can pay for this and
+     make change
+     1. pass in product id done
+     2. get price of product
+           do we have the product in our local inventory?
+            if not return false if so get price
+     3. assuming user insert money check against temp cb balance
+        validateChange method
+     4. makeChange method
      5. update inventory (to be written inside of product and vendingmachine)
      */
+     double prodPrice = findProduct(pid);
+     if (prodPrice > 0)
+     {
+         //check price against what user put in for money
+         if (prodPrice >= tcbox.getBalance())
+                 {
+                     /*can I make change?*/
+                     if (cbox.validateChange(tc.balance(), prodPrice))
+                     {
+                         tcbox=box.makeChange(tcbox.getBalance(), prodPrice);                      //call cashbox method to make change - update cashbox and also return a cashbox
+                        //tell user how much change 
+                        //update quantity
+                         //call update quantity method
+                         //loop through array of local products
+                         //once you find local product with pid
+                         //update quantity of that local product
+                     }
+                     else
+                         return false;
+                 }
+         else
+             return false;
+     }
+     else
+         return false;
+     return true;
+    }
 }
